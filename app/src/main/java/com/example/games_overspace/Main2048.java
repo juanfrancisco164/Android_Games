30dcae3862d5ac8328f9473a4bdce13f87214d9f
juanf <juanfrannet7@gmail.com>
2023-11-16 18:13:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/games_overspace/Main2048.java
@@ -0,0 +1,14 @@
+package com.example.games_overspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2048 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
